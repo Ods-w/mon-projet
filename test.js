ac7aee9f84478892c5802be926170e7acf9f4fba
WISSAL OUADOSS <wissalouadoss@gmail.com>
2026-04-07 15:46:44 +0100

    feat: ajout de test.js et b.txt pour la fonctionnalité AI C20

---
+++ test.js
@@ -0,0 +1,4 @@
+const { sumArray } = require('./utils');  // Assurez-vous que le chemin vers utils.js est correct
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(sumArray(numbers));  // Devrait afficher 15
